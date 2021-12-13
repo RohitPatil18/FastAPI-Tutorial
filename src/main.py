533c8b9cd2c3cfd1880b13e73644f15d40dc860f
Rohit Patil <patil.rohit048@gmail.com>
2021-12-13 19:02:13 +0530

    Path Parameters

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+class Roles(str, Enum):
+    admin = "ADMIN"
+    staff = "STAFF"
+    superuser = "SUPERUSER"
+
+
+@app.get("/")
 async def index():
-    return {'message': 'Hello world!'}
+    return {"message": "Hello world!"}
+
+@app.get("/greet/{username}")
+async def greetings(username: str):
+    return {"message": f"Hello, {username}!"}
+
+@app.get("/roles/{role}")
+async def check_user(role: Roles):
+    if role == Roles.admin:
+        return {"message": "Can't connect to Admin."}
+    elif role.value == Roles.staff.value:
+        return {"message": "Staff will contact you."}
+    else:
+        return {"message": "Permission denied."}
